f9e37e5124671fdd10efe869fe419e208e64e4d4
Chris-dagostino <dagostino.christian94@gmail.com>
2022-04-07 19:16:44 +0200

    post validation + modifiche

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Category;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Post;
+use Doctrine\Inflector\Rules\Word;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -26,7 +29,8 @@ class PostController extends Controller
      */
     public function create()
     {
-        return view('admin.post.create');
+        $categories = Category::all();
+        return view('admin.post.create', compact('categories'));
     }
 
     /**
@@ -37,7 +41,35 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        
+        $request->validate(
+            [
+                'title' => 'required|min:5',
+                'content' => 'required|min:10',
+                'category_id' => 'nullable|exists:categories,id'
+            ]
+        );
+
+
+        $data = $request->all();
+
+        $slug = Str::slug($data['title']);
+
+        $counter = 1;
+        while (Post::where('slug', $slug)->first()) {
+
+            $slug = Str::slug($data['title']) . '-' . $counter;
+            $counter++;
+        }
+
+        $data['slug'] = $slug;
+
+        $post = new Post();
+
+        $post->fill($data);
+
+        $post->save();
+
+        return redirect()->route('admin.posts.index');
     }
 
     /**
@@ -46,9 +78,9 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Post $post)
     {
-        //
+        return view('admin.post.show', compact('post'));
     }
 
     /**
@@ -57,9 +89,11 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Post $post)
     {
-        //
+
+        $categories = Category::all();
+        return view('admin.post.edit', compact('post', 'categories'));
     }
 
     /**
@@ -69,9 +103,42 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Post $post)
     {
-        //
+        $request->validate(
+            [
+                'title' => 'required|min:5',
+                'content' => 'required|min:10'
+            ]
+        );
+
+
+        $data = $request->all();
+
+        $slug = Str::slug($data['title']);
+
+        if ($post->slug != $slug ){
+            
+            $counter = 1;
+            while (Post::where('slug', $slug)->first()) {
+
+                $slug = Str::slug($data['title']) . '-' . $counter;
+                $counter++;
+            }
+
+            $data['slug'] = $slug;
+
+        }
+
+        
+
+       
+
+        $post->update($data);
+
+        $post->save();
+
+        return redirect()->route('admin.posts.index');
     }
 
     /**
@@ -80,8 +147,9 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return redirect()->route('admin.posts.index');
     }
 }
