215c2745999fb7b16f01ce66702d6c1df6456988
Chris-dagostino <dagostino.christian94@gmail.com>
2022-04-07 16:26:49 +0200

    first commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>work in progress</h1>
+        <h3>torna a trovarci</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+
+}
+</script>
+
+<style>
+
+</style>
